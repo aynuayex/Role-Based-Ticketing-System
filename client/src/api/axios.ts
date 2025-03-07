6df3a53be4926d8bf873a310886c2fe25a4d666d
Ayne Abreham Alemayehu <aynuman19@gmail.com>
2025-03-07 12:10:02 +0300

    feat: added vercel deployment config for backend api

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 export const API_BASE_URL = import.meta.env.VITE_BASE_API
-// export const API_BASE_URL = "http://localhost:5000/api/v1"
-// export const API_BASE_URL = "https://pizza-ordering-service-5ygp.vercel.app/api/v1"
 
 export default axios.create({
     baseURL: API_BASE_URL,
