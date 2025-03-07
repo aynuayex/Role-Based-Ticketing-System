60ddba2c01d9260538b07ec7233d31ef2ad6a13d
Ayne Abreham Alemayehu <aynuman19@gmail.com>
2025-03-07 12:38:20 +0300

    feat: updated allowed origins

--- server/src/config/allowedOrigins.js
+++ server/src/config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
-    // "http://192.168.8.3:5173",
-    "http://localhost:5173",
-    // "http://localhost:4173",
-    "https://hosted_web_domain.vercel.app"
-  ];
-  
-  module.exports = allowedOrigins;
+  // "http://192.168.8.3:5173",
+  "http://localhost:5173",
+  // "http://localhost:4173",
+  process.env.FRONTEND_URL,
+];
+
+module.exports = allowedOrigins;
