d321cf49b3b9a0922b501b74b2883bbdb152213a
Ayne Abreham Alemayehu <aynuman19@gmail.com>
2025-03-11 16:46:04 +0300

    feat: updated logger

--- server/src/config/logger.js
+++ server/src/config/logger.js
@@ -11,24 +11,31 @@ if (process.env.NODE_ENV === "development") {
   }
 }
 
-const transports = [
-  new winston.transports.Console(),
-];
-
-// Add file transport only in development
-if (process.env.NODE_ENV === "development") {
-  transports.push(new winston.transports.File({ filename: path.join(logDir, "app.log") }));
-}
-
+// create the logger
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.printf(({ timestamp, level, message }) => {
-      return `${timestamp} [${level.toUpperCase()}]: ${message}`;
-    })
-  ),
-  transports,
+    winston.format.errors({ stack: true }),
+    // winston.format.colorize(),
+    winston.format.timestamp({
+      format: 'MM-DD-YYYY HH:mm:ss [[' + global.process.pid + ']]'
+    }),
+    winston.format.printf(info => {
+      if (info.stack) {
+        return `${info.timestamp} [${info.level.toUpperCase()}]: ${info.message} - ${info.stack}`
+      }
+
+      return `${info.timestamp} [${info.level.toUpperCase()}]: ${info.message}`
+    }
+  )),
+  transports: [
+    new winston.transports.Console(),
+  ],
 });
+
+// Add file transport only in development
+if (process.env.NODE_ENV === "development") {
+  logger.add(new winston.transports.File({ filename: path.join(logDir, "app.log") }));
+}
 
 module.exports = logger;
